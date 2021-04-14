af6491ef00996e96ea8632d90370690ccfe9bf88
omar-aq <omar.1997aq@gmail.com>
2021-04-14 19:43:05 +0300

    newslatter

--- my-app/src/components/Footer.js
+++ my-app/src/components/Footer.js
@@ -127,7 +127,7 @@ const Footer = () => {
               </Typography>
             </Grid>
             <Grid item style={{ paddingTop: "20px" }}>
-              <ButtonGroup style={{ width: "100px" }}>
+              <ButtonGroup style={{ width: "80%" }}>
                 <div className={classes.subInput}>
                   <div className={classes.iconInput}>
                     <EmailOutlinedIcon style={{ color: "#92a2ad" }} />
